15a3c1af95938ec0e177521ba7fbe98f71369af3
Gonçalo Silva <goncalossilva@gmail.com>
2014-03-23 04:41:44 +0000

    Add methods for registering/unregistering media parsers

--- src/main/java/com/todoist/mediaparser/MediaParser.java
+++ src/main/java/com/todoist/mediaparser/MediaParser.java
@@ -5,14 +5,12 @@ import com.todoist.mediaparser.util.MimeUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
 
 public abstract class MediaParser {
-    private static final List<Class<? extends MediaParser>> MEDIA_PARSERS =
-            new ArrayList<Class<? extends MediaParser>>() {{
+    private static final LinkedHashSet<Class<? extends MediaParser>> sMediaParsers =
+            new LinkedHashSet<Class<? extends MediaParser>>() {{
                 add(ImglyParser.class);
                 add(InstagramParser.class);
                 add(TwitpicParser.class);
@@ -36,7 +34,7 @@ public abstract class MediaParser {
      * Returns an appropriate {@link MediaParser} instance for this {@code url}, or {code null} if not supported.
      */
     public static MediaParser getInstance(String url) {
-        for(Class<? extends MediaParser> mediaParserClass : MEDIA_PARSERS) {
+        for(Class<? extends MediaParser> mediaParserClass : sMediaParsers) {
             try {
                 MediaParser mediaParser = mediaParserClass.getDeclaredConstructor(String.class).newInstance(url);
                 if(mediaParser.matches())
@@ -48,14 +46,38 @@ public abstract class MediaParser {
         return null;
     }
 
+	/**
+	 * Registers a {@link MediaParser} subclass. It will be checked when {@link #getInstance(String)} is invoked.
+	 *
+	 * @return true if it was registered, false if it was already registered.
+	 */
+	public static boolean registerMediaParser(Class<? extends MediaParser> mediaParserClass) {
+		return sMediaParsers.add(mediaParserClass);
+	}
+
+	/**
+	 * Unregisters a {@link MediaParser} subclass. It will no longer be checked when {@link #getInstance(String)} is
+	 * invoked.
+	 *
+	 * @return true if it was removed, false if it was not registered.
+	 */
+	public static boolean unregisterMediaParser(Class<? extends MediaParser> mediaParserClass) {
+		return sMediaParsers.remove(mediaParserClass);
+	}
+
     MediaParser(String url) {
         mUrl = url;
     }
 
 	/**
+	 * Returns the original url.
+	 */
+	public final String getUrl() {
+		return mUrl;
+	}
+
+	/**
 	 * Returns the url for this media. Redirects might need to be followed.
-	 *
-	 * This is not guaranteed to be fast, thus should be called from a background thread. Cannot be null.
 	 */
 	public final String getContentUrl() {
 		if(mContentUrl == null)
@@ -112,8 +134,6 @@ public abstract class MediaParser {
 			try {
 				in = new URL(urlString).openStream();
 				mimeType = MimeUtils.guessContentTypeFromStream(in);
-			} catch(MalformedURLException e) {
-				e.printStackTrace();
 			} catch(IOException e) {
 				e.printStackTrace();
 			} finally {

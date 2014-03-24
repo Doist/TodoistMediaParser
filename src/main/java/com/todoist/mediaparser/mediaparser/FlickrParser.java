a902f5f3fd273f3946bda10bf2d606f839b59a08
Gonçalo Silva <goncalossilva@gmail.com>
2014-03-24 12:03:48 +0000

    Use flickr's full image for the thumbnail, if available
    Flickr's thumbnails are just too small at 75x75.

--- src/main/java/com/todoist/mediaparser/mediaparser/FlickrParser.java
+++ src/main/java/com/todoist/mediaparser/mediaparser/FlickrParser.java
@@ -28,6 +28,16 @@ public class FlickrParser extends BaseOEmbedMediaParserWithContent {
 	}
 
 	@Override
+	protected String createThumbnailUrl(int smallestSide) {
+		// Flickr thumbnails are tiny. If available, use content url.
+		String contentUrl = super.getContentUrl();
+		if(!mUrl.equals(contentUrl))
+			return contentUrl;
+		else
+			return super.createThumbnailUrl(smallestSide);
+	}
+
+	@Override
 	protected String getOEmbedUrlTemplate() {
 		return "http://www.flickr.com/services/oembed?url=%s&format=json";
 	}
